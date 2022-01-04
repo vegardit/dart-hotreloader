351ea8672189000c1e9b08a0c9d43772def14d4e
sebthom <sebthom@users.noreply.github.com>
2022-01-04 19:50:07 +0100

    v3.0.2

--- .ci/release-trigger.sh
+++ .ci/release-trigger.sh
@@ -1,4 +1,4 @@
 # this file is evaluated by .ci/build.sh
 
- RELEASE_VERSION="3.0.1" # if version pubspec.yaml contains this version, then perform a release
-NEXT_DEV_VERSION="3.0.2-SNAPSHOT" # increment to this dev version
+ RELEASE_VERSION="3.0.2" # if version pubspec.yaml contains this version, then perform a release
+NEXT_DEV_VERSION="3.0.3-SNAPSHOT" # increment to this dev version
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 
+## [3.0.2] - 2022-01-4
+
+### Changed
+- support vm_service 7.x
+
+
 ## [3.0.1] - 2021-04-18
 
 ### Changed
