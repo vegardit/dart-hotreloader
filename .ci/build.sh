3880629e2657cd7783d50a547095b35e2cb2c8d8
sebthom <sebthom@users.noreply.github.com>
2022-03-20 21:18:08 +0100

    Update copyright notice

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright 2020-2021 by Vegard IT GmbH, Germany, https://vegardit.com
+# Copyright 2020-2022 by Vegard IT GmbH, Germany, https://vegardit.com
 # SPDX-License-Identifier: Apache-2.0
 #
 # @author Sebastian Thomschke, Vegard IT GmbH
--- example/tool/run.sh
+++ example/tool/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2020-2021 by Vegard IT GmbH (https://vegardit.com) and contributors.
+# Copyright 2020-2022 by Vegard IT GmbH (https://vegardit.com) and contributors.
 # SPDX-License-Identifier: Apache-2.0
 #
 # @author Sebastian Thomschke, Vegard IT GmbH
